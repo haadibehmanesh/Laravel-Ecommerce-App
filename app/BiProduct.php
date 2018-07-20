a247b4c4fe15a59d3613811f6d13809b738c32cd
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-07-20 14:17:08 +0430

    modify product category

--- app/BiProduct.php
+++ app/BiProduct.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class BiProduct extends Model
 {
-    
+    public function categories()
+    {
+        return $this->belongsToMany('App\BiCategory');
+    }
 }
