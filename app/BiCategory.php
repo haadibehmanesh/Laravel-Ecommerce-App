12eaeb305ae2c734abdf2733173409b43f272178
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-07-20 14:17:24 +0430

    MOdify bi category

--- app/BiCategory.php
+++ app/BiCategory.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class BiCategory extends Model
 {
+    Protected $table = "bi_categories";
+
+    public function products()
+    {
+        return $this->belongsToMany('App\BiProduct');
+    }
     
 }
