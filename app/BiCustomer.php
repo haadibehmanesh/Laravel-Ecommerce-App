c609b1669a521a8b242790a05f53dc7086405912
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-07-22 18:35:14 +0430

    create bi customer

--- app/BiCustomer.php
+++ app/BiCustomer.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class BiCustomer extends Model
 {
-    //
+    protected $table = 'bi_customers';
+
+    public function cgroups()
+    {
+        return $this->belongsToMany('App\BiCGroup');
+    }
 }
