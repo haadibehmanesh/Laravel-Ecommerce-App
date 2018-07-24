1106c043cafa26ef8e895384fee0479d253706ad
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-07-24 12:49:57 +0430

    create BiMerchant and modify

--- app/BiMerchant.php
+++ app/BiMerchant.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class BiMerchant extends Model
 {
-    
+    protected $table = 'bi_merchants';
+
+    public function m_groups()
+    {
+        return $this->belongsToMany('App\BiMGroup');
+    }
 }
