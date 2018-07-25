857ca4d17d176a99d44198fa9bf60cfc7b005143
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-07-25 20:54:42 +0430

    Add constructor remove old cart list

--- app/Model/Checkout/BiCart.php
+++ app/Model/Checkout/BiCart.php
@@ -12,5 +12,16 @@ class BiCart extends Model
      * @var string
      */
     protected $table = 'bi_cart';
+
+    public function __construct() 
+    {
+        $date = new \DateTime();
+        $date->modify('-1 hours');
+        $formatted_date = $date->format('Y-m-d H:i:s');
+        
+        $this->where(function ($query) {
+            $query->where('api_id', '>', 0)->orWhere('customer_id', 0);
+        })->where('created_at', '<', $formatted_date)->delete();
+    } 
     
 }
