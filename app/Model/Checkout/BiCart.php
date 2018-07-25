feaec85c44f8df1704ea94216ef702e92ed946a4
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-07-25 19:57:07 +0430

    Associate bi_cart table

--- app/Model/Checkout/BiCart.php
+++ app/Model/Checkout/BiCart.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class BiCart extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bi_cart';
+    
 }
