4f1022b1ce6bf693642d218ad6e54926d2de6fb1
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-07-25 20:24:27 +0430

    Add BiCart use

--- app/Http/Controllers/Catalog/Checkout/CartController.php
+++ app/Http/Controllers/Catalog/Checkout/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Catalog\Checkout;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\Checkout\BiCart;
 
 class CartController extends Controller
 {
