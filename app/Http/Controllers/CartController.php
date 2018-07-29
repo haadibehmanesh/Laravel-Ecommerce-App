f6f3584f1413016d76100db3d4142091998bcb71
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-07-29 16:12:43 +0430

    add Cart provider and complete update and delete functions

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -35,7 +36,7 @@ class CartController extends Controller
     public function store(Request $request)
     {
         cart::add($request->id, $request->name, 1, $request->price)->associate('App\BiProduct');
-
+      
         return redirect()->route('cart.index')->with('success_message', 'Item was added to your cart');
     }
 
@@ -70,7 +71,18 @@ class CartController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'quantity' => 'required|numeric|between:1,5'
+        ]);
+
+        if ($validator->fails()) {
+            session()->flash('errors', collect(['Quantity must be between 1 and 5.']));
+            return response()->json(['success' => false], 400);
+        }
+
+        Cart::update($id, $request->quantity);
+        session()->flash('success_message', 'Quantity was updated successfully!');
+        return response()->json(['success' => true]);
     }
 
     /**
@@ -80,7 +92,9 @@ class CartController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
-    {
-        //
+    {   
+        Cart::remove($id);
+
+        return back()->with('success_message', 'Item has been removed!');
     }
 }
