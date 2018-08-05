65e7a156fc4d60e659aea08e852a0996407fd22d
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-08-05 18:49:11 +0430

    create order function for item model

--- app/BiOrderItem.php
+++ app/BiOrderItem.php
@@ -10,4 +10,9 @@ class BiOrderItem extends Model
     protected $table = 'bi_order_items';
 
     protected $fillable = ['name', 'price', 'quantity', 'total', 'bi_product_id', 'code'];
+
+    public function order ()
+    {
+        return $this->belongsTo('App\BiOrder', 'bi_order_id');
+    }
 }
