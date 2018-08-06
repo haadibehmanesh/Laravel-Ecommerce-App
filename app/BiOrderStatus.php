9d0b2548ceceb4310d7962021c7c55be7a35a6a5
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-08-06 19:15:00 +0430

    add order function to order status model

--- app/BiOrderStatus.php
+++ app/BiOrderStatus.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class BiOrderStatus extends Model
 {
-    
+    protected $table = 'bi_order_status';
+
+    protected $fillable = ['name'];
+
+    public function Orders ()
+    {
+        return $this->hasMany('App\BiOrder');
+    }
 }
