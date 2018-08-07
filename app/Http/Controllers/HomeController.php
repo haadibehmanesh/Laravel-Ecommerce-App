9f79e1e9969ea37db4461c21493f10dc7476a3f8
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-08-07 12:05:49 +0430

    change homecontroller to mainpagecontroller because of auth makes it

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,96 +1,28 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use App\BiProduct;
-use App\BiCategory;
-use App\BiSlider;
-use App\BiSliderImage;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {   
-        $allcategories = BiCategory::orderBy('sort_order', 'asc')->get();
-      
-        $slider = BiSlider::where('name' , 'index')->get();
-     
-        $sliderimages = BiSliderImage::where('bi_slider_id', $slider[0]->id)->get();
-        
-        return view('layouts/home')->with([
-            'allcategories' => $allcategories,
-            'sliderimages' =>  $sliderimages
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return view('home');
     }
 }
