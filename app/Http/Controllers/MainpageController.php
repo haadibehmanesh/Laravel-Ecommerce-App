027027b76704a2b6c292581614f1537fca1915e7
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-08-07 12:06:16 +0430

    main page controller instead of home controller

---
+++ app/Http/Controllers/MainpageController.php
@@ -0,0 +1,96 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\BiProduct;
+use App\BiCategory;
+use App\BiSlider;
+use App\BiSliderImage;
+use Illuminate\Http\Request;
+
+class MainpageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+        $allcategories = BiCategory::orderBy('sort_order', 'asc')->get();
+      
+        $slider = BiSlider::where('name' , 'index')->get();
+     
+        $sliderimages = BiSliderImage::where('bi_slider_id', $slider[0]->id)->get();
+        
+        return view('layouts/mainpage')->with([
+            'allcategories' => $allcategories,
+            'sliderimages' =>  $sliderimages
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
