0c42b9815bc3ad91e45a14d2756058b0d78b1835
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-08-16 14:15:11 +0430

    show subcategory in category view by ajax

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\BiProduct;
+use App\BiCategory;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class AjaxController extends Controller {
+    
+    public function getCategory($slug)
+    {  
+        $pagination = 9;
+        
+        $slug_db = explode('/', $slug);
+       
+        $category = BiCategory::where('slug', $slug_db)->firstOrFail();
+       
+      
+        $productsForCategories = $category->products()->orderBy('id', 'desc')->paginate($pagination);
+        $allcategories = BiCategory::orderBy('sort_order', 'asc')->get();
+        
+        return view('layouts/categories/ajaxcat')->with([
+            'category' => $category,
+            'productsForCategories' => $productsForCategories,
+            'allcategories' => $allcategories,
+        ])->render();
+        
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
