2f23037627236cb565e46aa5e1640a36eaa114c8
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-08-20 14:00:34 +0430

    make customer auth by multiauth and 21 files change and create

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,17 @@ Route::post('/getcategory/{category}','AjaxController@getCategory');
 Route::post('/getlist/{category}','AjaxController@getList');
 Route::post('/ajax/products', 'AjaxController@getProduct');
 
+
+Route::group(['prefix' => 'customer'], function () {
+  Route::get('/login', 'CustomerAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'CustomerAuth\LoginController@login');
+  Route::post('/logout', 'CustomerAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'CustomerAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'CustomerAuth\RegisterController@register');
+
+  Route::post('/password/email', 'CustomerAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'CustomerAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'CustomerAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'CustomerAuth\ResetPasswordController@showResetForm');
+});
