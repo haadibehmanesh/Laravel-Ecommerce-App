d59c1f1d8f5f53fb0f112eef8752614dd0601f4d
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-08-23 15:16:52 +0430

    add customer relationship tp bicustomer

--- app/BiCustomer.php
+++ app/BiCustomer.php
@@ -7,9 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class BiCustomer extends Model
 {
     protected $table = 'bi_customers';
+    protected $fillable = [
+        'customer_id',
+    ];
 
     public function c_groups()
     {
         return $this->belongsToMany('App\BiCGroup');
+    }
+
+    public function customer(){
+
+        return $this->belongsTo('App\Customer');
     }
 }
