2c1b50e5d296900279ab6896c827c2d367ea36d2
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-08-24 12:37:49 +0430

    add searchable traits and model to BiProduct model

--- app/BiProduct.php
+++ app/BiProduct.php
@@ -3,10 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class BiProduct extends Model
 {
+
+    use SearchableTrait;
+
+    /**
+     * Searchable rules.
+     *
+     * @var array
+     */
+    protected $searchable = [
+        /**
+         * Columns and their priority in search results.
+         * Columns with higher values are more important.
+         * Columns with equal values have equal importance.
+         *
+         * @var array
+         */
+        'columns' => [
+            'bi_products.name' => 10,
+
+            
+        ],
+
+    ];
+
     public function categories()
     {
         return $this->belongsToMany('App\BiCategory');
