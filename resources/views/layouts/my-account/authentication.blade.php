28cb14a7fc52cf0287e112bc720ed0c06a8719b6
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-11-18 11:44:38 +0330

    redirect back

--- resources/views/layouts/my-account/authentication.blade.php
+++ resources/views/layouts/my-account/authentication.blade.php
@@ -290,7 +290,13 @@ var _zxcvbnSettings = {"src":"http:\/\/localhost\/takhfiftest\/wp-includes\/js\/
 <h2>ورود</h2>
 <form class="form-horizontal" role="form" method="POST" action="{{ url('/customer/login') }}">
     {{ csrf_field() }}
-
+    <div class="form-group">
+    @if (request()->has('previous'))
+        <input type="hidden" name="previous" value="{{ request()->get('previous') }}">
+    @else
+        <input type="hidden" name="previous" value="{{ url()->previous() }}">
+    @endif
+</div>
     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
         <label for="email" class="col-md-4 control-label"> شماره همراه یا آدرس ایمیل</label>
 
