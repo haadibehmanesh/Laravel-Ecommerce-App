2db33b6104ecf17715d934e5cd2c32028616973f
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-11-19 21:53:56 +0330

    update footer and header

--- resources/views/layouts/aboutus.blade.php
+++ resources/views/layouts/aboutus.blade.php
@@ -116,12 +116,13 @@ function toPersianNum( num, dontTrim ) {
                 {{--<li id="menu-item-166" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-166"><a href="/products">همه پیشنهادها</a></li>--}}
             </ul>
             <!--phone-->
-            {{--<div class="phone"><span><i class="fa fa-book"></i>بانک جامع اطلاعاتی</span></div>--}}
+            <div class="phone"><span><i class="fa fa-phone"></i>09176952155 - 07136265496</span></div>
 			            
 			<!--social-->
             <div class="social_header">
-                <a href="" title="تلگرام"><i class="fa fa-send-o"></i></a>
-                <a href="#" title="اینستاگرام"><i class="fa fa-instagram"></i></a>
+            {{--    <a href="" title="تلگرام"><i class="fa fa-send-o"></i></a>--}}
+                <a href="https://www.instagram.com/boninjaa/" title="اینستاگرام"><i class="fa fa-instagram"></i></a>
+                
             </div>
         </div>
         
@@ -343,10 +344,8 @@ function toPersianNum( num, dontTrim ) {
 
             <!--social-->
             <div class="social_footer">
-                <a target="_blank" href="#" title="" class="telegram"></a>
-                <a target="_blank" href="#" title="" class="instagram"></a>
-                <a target="_blank" href="#" title="" class="facebook"></a>
-            </div>
+                    <a target="_blank" href="https://www.instagram.com/boninjaa" title="" class="instagram"></a>
+                </div>
 
            <!--concession-->
            <div class="concession">
@@ -381,15 +380,13 @@ function toPersianNum( num, dontTrim ) {
                 </div>--}}
                 <!--Service-->
                 <div class="col-lg-5 col-md-5 block_service">
-                <div class="service"><span>درباره بن اینجا</span><div class="menu-%d8%b1%d8%a7%d9%87%d9%86%d9%85%d9%80%d8%a7-container"><ul id="menu-%d8%b1%d8%a7%d9%87%d9%86%d9%85%d9%80%d8%a7" class="menu"><li id="menu-item-187" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-187"><a href="{{route('aboutus.index')}}">درباره ما</a></li>
-                    <li id="menu-item-188" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-188"><a href="#">قوانین و مقررات</a></li>
-                    <li id="menu-item-191" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-191"><a href="#">تماس با ما</a></li>
-                    </ul></div></div><div class="service"><span>راهنما</span><div class="menu-%d8%b1%d8%a7%d9%87%d9%86%d9%85%d9%80%d8%a7-container"><ul id="menu-%d8%b1%d8%a7%d9%87%d9%86%d9%85%d9%80%d8%a7-1" class="menu"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-187"><a href="#">آموزش مفید</a></li>
-                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-188"><a href="#">نحوه خرید</a></li>
-                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-189"><a href="#">نحوه فروش</a></li>
-                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-190"><a href="#">کجا پیدا میشه</a></li>
-                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-191"><a href="#">تماس با ما</a></li>
-                    </ul></div></div>            </div>
+                        <div class="service"><span>درباره بن اینجا</span><div class="menu-%d8%b1%d8%a7%d9%87%d9%86%d9%85%d9%80%d8%a7-container"><ul id="menu-%d8%b1%d8%a7%d9%87%d9%86%d9%85%d9%80%d8%a7" class="menu"><li id="menu-item-187" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-187"><a href="{{route('aboutus.index')}}">درباره ما</a></li>
+                            <li id="menu-item-188" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-188"><a href="{{route('cooperation.index')}}">همکاری با بن اینجا</a></li>
+                            <li id="menu-item-191" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-191"><a href="{{route('contactus.index')}}">تماس با ما</a></li>
+                            </ul></div></div><div class="service"><span>راهنما</span><div class="menu-%d8%b1%d8%a7%d9%87%d9%86%d9%85%d9%80%d8%a7-container"><ul id="menu-%d8%b1%d8%a7%d9%87%d9%86%d9%85%d9%80%d8%a7-1" class="menu">
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-188"><a href="#">نحوه خرید</a></li>
+                            
+                            </ul></div></div>            </div>
                 <!--map-->
                 <div class="col-lg-3 col-md-3">
                     <div class="map">
