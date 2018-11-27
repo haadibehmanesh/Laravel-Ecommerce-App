de4f20ff08f8617d69196dea35e397d89286ed01
Hadi Behmanesh <haadibehmanesh@gmail.com>
2018-11-27 12:34:45 +0330

    add google tag manager to views

--- resources/views/layouts/checkout/bankresult.blade.php
+++ resources/views/layouts/checkout/bankresult.blade.php
@@ -4,6 +4,16 @@
 <!-- Boninja.com -->
 <!-- Boninja.com --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Boninja.com -->
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129893987-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129893987-1');
+</script>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
