0f9e9de9f4c33ea9e36073c923912f85d0588c48
Hadi Behmanesh <haadibehmanesh@gmail.com>
2019-02-13 12:18:40 +0330

    add expires_in to token

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\Customer; 
 use Illuminate\Support\Facades\Auth; 
 use Validator;
+use Carbon\Carbon;
 class CustomerController extends Controller 
 {
 public $successStatus = 200;
@@ -14,12 +15,13 @@ public $successStatus = 200;
      * @return \Illuminate\Http\Response 
      */ 
     public function login(){ 
-        //dd('hi');
+        
         if(Auth::guard('customer')->attempt(['phone' => request('phone'), 'password' => request('password')])){ 
             $user = Auth::guard('customer')->user(); 
-           // dd($user);
-            $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-            return response()->json(['success' => $success], $this-> successStatus); 
+            $objToken = $user->createToken('MyApp');
+            $success['token'] = $objToken-> accessToken; 
+            $expiration = $objToken->token->expires_at->diffInSeconds(Carbon::now()); 
+            return response()->json(['success' => $success,"expires_in" => $expiration], $this-> successStatus); 
         } 
         else{ 
             return response()->json(['error'=>'Unauthorised'], 401); 
@@ -33,20 +35,33 @@ public $successStatus = 200;
     public function register(Request $request) 
     { 
         $validator = Validator::make($request->all(), [ 
-           // 'name' => 'required', 
-            //'email' => 'required|email', 
-            //'password' => 'required', 
+            'name' => 'required', 
+            'phone' => 'required', 
+            'password' => 'required', 
             //'c_password' => 'required|same:password', 
         ]);
 if ($validator->fails()) { 
             return response()->json(['error'=>$validator->errors()], 401);            
         }
-$input = $request->all(); 
-        $input['password'] = bcrypt($input['password']); 
-        $user = Customer::create($input); 
-        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-        $success['name'] =  $user->name;
-return response()->json(['success'=>$success], $this-> successStatus); 
+        $hasBeen = Customer::where('phone',$request->phone)->first();
+        if(empty($hasBeen)){
+            $input = $request->all(); 
+            $input['password'] = bcrypt($input['password']); 
+            $user = Customer::create($input); 
+
+            $objToken = $user->createToken('MyApp');
+            $success['token'] = $objToken-> accessToken; 
+            $expiration = $objToken->token->expires_at->diffInSeconds(Carbon::now());
+            
+
+           // $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+            $success['name'] =  $user->name;
+            return response()->json(['success' => $success,"expires_in" => $expiration], $this-> successStatus);
+           //return response()->json(['success'=>$success], $this-> successStatus); 
+        }else{
+            return response()->json(['error'=>'hasBeen'], 401);
+        }
+        
     }
 /** 
      * details api 
@@ -56,7 +71,6 @@ return response()->json(['success'=>$success], $this-> successStatus);
     public function details() 
     { 
         $user = Auth::guard('api')->user();
-        //dd($user); 
         return response()->json(['success' => $user], $this-> successStatus); 
     } 
 }
