eea815ac48764ea053152a8fd6a11c7c97bfe1c9
Hadi Behmanesh <haadibehmanesh@gmail.com>
2019-04-10 15:14:50 +0430

    add call back to api and blade

---
+++ resources/views/layouts/api/callback.blade.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="fa_IR">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+
+<head>
+</head>
+<!DOCTYPE html>
+<html lang="fa_IR">
+
+<!-- Boninja.com -->
+<!-- Boninja.com --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Boninja.com -->
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129893987-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129893987-1');
+</script>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>
+        سبد خرید    </title>
+	<link rel='dns-prefetch' href='/cart' />
+		
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel="shortcut icon" href="{{{ asset('img/favicon.png') }}}">
+<link rel='stylesheet' id='validate-engine-css-css'  href={{ asset('wp-content/plugins/wysija-newsletters/css/validationEngine.jquery4dc3.css?ver=2.8.2') }} type='text/css' media='all' />
+<link rel='stylesheet' id='select2-css'  href={{ asset('wp-content/plugins/woocommerce/assets/css/select26765.css?ver=3.3.3') }} type='text/css' media='all' />
+<link rel='stylesheet' id='woocommerce-layout-rtl-css'  href={{ asset('wp-content/plugins/woocommerce/assets/css/woocommerce-layout-rtl6765.css?ver=3.3.3') }} type='text/css' media='all' />
+<link rel='stylesheet' id='woocommerce-smallscreen-rtl-css'  href={{ asset('wp-content/plugins/woocommerce/assets/css/woocommerce-smallscreen-rtl6765.css?ver=3.3.3') }} type='text/css' media='only screen and (max-width: 768px)' />
+<link rel='stylesheet' id='woocommerce-general-rtl-css'  href={{ asset('wp-content/plugins/woocommerce/assets/css/woocommerce-rtl6765.css?ver=3.3.3') }} type='text/css' media='all' />
+<link rel='stylesheet' id='megamenu-css'  href={{ asset('wp-content/uploads/maxmegamenu/style3d1a.css?ver=1.1') }} type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href={{ asset('wp-includes/css/dashicons.min1845.css?ver=4.9.6') }} type='text/css' media='all' />
+<link rel='stylesheet' id='dokan-style-css'  href={{ asset('wp-content/plugins/dokan-lite/assets/css/styleb246.css?ver=2.7.8') }} type='text/css' media='all' />
+<link rel='stylesheet' id='dokan-fontawesome-css'  href={{ asset('wp-content/plugins/dokan-lite/assets/vendors/font-awesome/font-awesome.minb246.css?ver=2.7.8') }} type='text/css' media='all' />
+<link rel='stylesheet' id='dokan-rtl-style-css'  href={{ asset('wp-content/plugins/dokan-lite/assets/css/rtlb246.css?ver=2.7.8') }} type='text/css' media='all' />
+<script type='text/javascript' src={{ asset('wp-includes/js/jquery/jqueryb8ff.js?ver=1.12.4')}}></script>
+<script type='text/javascript' src={{ asset('wp-includes/js/jquery/jquery-migrate.min330a.js?ver=1.4.1')}}></script>
+    <!-- Bootstrap -->
+    <link href={{ asset('wp-content/themes/takhfifat/css/bootstrap.min.css')}} rel="stylesheet">
+    <link href={{ asset('wp-content/themes/takhfifat/css/bootstrap-rtl.css')}} rel="stylesheet">
+    <link href={{ asset('wp-content/themes/takhfifat/css/font-awesome.css')}} rel="stylesheet">
+    <link href={{ asset('wp-content/themes/takhfifat/stylefc99.css?ver=2.9')}} rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="http://localhost/takhfiftest/wp-content/themes/takhfifat/js/html5shiv.min.js"></script>
+    <script src="http://localhost/takhfiftest/wp-content/themes/takhfifat/js/respond.min.js"></script>
+    <![endif]-->
+
+            {{--	<style>
+	span.price del:before{ content:'قیمت: '; }
+	span.price ins:before{ content:'با تخفیف: '; }
+			.phone,
+	input[type="submit"], input[type="button"], button,
+	.nav-tabs>li.active>a .fa, .nav-tabs>li.active>a:focus .fa, .nav-tabs>li.active>a:hover .fa,
+	.eye_buy a,
+	.price span.with_discount,
+	.title_post span.like,
+	.block_gallery .content_item .Slogan span,
+	.content_item .Discount:before,
+	.box_offer .Discount:before,
+	.box_offer .price del,
+	.title_post .Discount:before,
+	span.number_items_cart,
+	p.buttons a,
+	.wc-proceed-to-checkout .checkout-button,
+	.woocommerce a.remove:hover,
+	.post-content input[type="submit"],
+	input[type="submit"].dokan-btn-theme, a.dokan-btn-theme, .dokan-btn-theme
+	{
+		background-color:#ff5a5f !important;
+	}
+	.box_offer .price del:after{
+		border-right-color: #ff5a5f;
+	}
+	input[type="submit"]:hover, input[type="button"]:hover, button:hover,
+	.eye_buy a:hover,
+	{
+		background-color:#ff5a5f	}
+	.content_item > span a .fa,
+	.box_single .title_block span,
+	.option_item_gallery span.address .fa,
+	.item_terms_use p .fa,
+	.box_offer span.address i,
+	.cart_list.product_list_widget li.mini_cart_item a.remove,
+	.woocommerce a.remove,
+	.label_map
+	{
+		color:#ff5a5f !important
+	}
+	.top_header,
+	.title_post span.like:before,
+	.top_header, 
+	.title_post span.like:before, 
+	.nav-tabs>li.active>a, 
+	.nav-tabs>li.active>a:hover,
+	.title_post,
+	.phone,
+	.label_map,
+	.post-content input[type="submit"],
+	input[type="submit"].dokan-btn-theme, a.dokan-btn-theme, .dokan-btn-theme
+	{
+		border-color:#ff5a5f	}	
+		</style>--}}
+	<script src={{ asset('wp-content/themes/takhfifat/js/parsinumber.min.js')}}></script>
+	<script>
+	function toPersianNum( num, dontTrim ) {
+		var i = 0,
+			dontTrim = dontTrim || false,
+			num = dontTrim ? num.toString() : num.toString().trim(),
+			len = num.length,
+			res = '',
+			pos,
+			persianNumbers = typeof persianNumber == 'undefined' ?
+				['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'] :
+				persianNumbers;
+		for (; i < len; i++)
+			if (( pos = persianNumbers[num.charAt(i)] ))
+				res += pos;
+			else
+				res += num.charAt(i);
+		return res;
+	}
+    </script>
+  <script src={{ asset('wp-content/themes/takhfifat/js/axios.js')}}></script>
+    <script>
+    jQuery(document).ready(function(){  
+        //alert('h');
+        jQuery('.select-quantity').change(function() {
+            var qty = jQuery(this).attr('value');
+            var id = jQuery(this).attr('data-id');
+          
+            axios.patch(`/cart/${id}`, {
+                        quantity: this.value
+                    })
+                    .then(function (response) {
+                        // console.log(response);
+                        window.location.href = '{{ route('cart.index') }}'
+                    })
+                    .catch(function (error) {
+                        // console.log(error);
+                        window.location.href = '{{ route('cart.index') }}'
+             });      
+        });
+    });
+    </script>
+
+<script>
+        jQuery(document).ready(function() {
+           
+             // grab the initial top offset of the navigation 
+                var stickyNavTop = jQuery('.nav').offset().top;
+                
+                // our function that decides weather the navigation bar should have "fixed" css position or not.
+                var stickyNav = function(){
+                 var scrollTop = jQuery(window).scrollTop(); // our current vertical position from the top
+                      
+                 // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+                 // otherwise change it back to relative
+                 if (scrollTop > stickyNavTop) { 
+                    jQuery('.nav').addClass('sticky');
+                 } else {
+                    jQuery('.nav').removeClass('sticky'); 
+                 }
+             };
+ 
+             stickyNav();
+             // and run it again every time you scroll
+             jQuery(window).scroll(function() {
+                 stickyNav();
+             });
+         });
+ </script>
+
+</head>
+<body class="rtl page-template-default page page-id-8 woocommerce-cart woocommerce-page mega-menu-main-menu dokan-theme-takhfifat">
+
+    <!------ Header
+------------------->
+<header>
+    <div class="container">
+        <div class="row">
+
+               
+                            <div class="logo"  style="margin:auto;margin-top:10px;width:100%"><h1><a href="#" title="سامانه خرید و تخفیف گروهی بن اینجا"></a></h1></div>
+        
+ 
+           
+            
+
+
+        </div>
+    </div>
+</header>
+<!-- / Header -->    <!--Nav-->
+
+<div class="clear"></div>
+<!-- / Nav -->    <!--wrapper-->
+    <section id="wrapper">
+    <div class="container">
+        <div class="row">
+           
+            <div class="post-content-page">
+                                        
+                    <!--title & discount & views-->
+                    <div class="title_post"  style="margin-top:30px;margin-bottom:50px">
+                        <h1>نتیجه تراکنش</h1>
+                    </div>
+                    @if(!empty($message))
+                   <span style="
+                   font: 18px/30px IRANSans;margin:auto;margin-top:10px;width:100%;
+               "> {!!$message!!}</span>
+        
+                    <button type="button" class="btn" style="margin:auto;width:100%;margin-top:30px;" value="" onclick="window.location.href='intent://com.example.boninja/#Intent;scheme=launch;package=com.example.boninja;S.content=WebContent;end'"><span style="
+                        font: 24px/30px IRANSans
+                    " > بازگشت به اپلیکیشن بن اینجا</span>
+               
+        </button>
+                    @elseif(!empty($error))
+                    <span style="
+                   font: 18px/30px IRANSans;margin:auto;margin-top:10px;width:100%;
+               ">    {!!$error!!}</span>
+        
+                    <button type="button" class="btn" style="margin:auto;width:100%;margin-top:30px;" value="" onclick="window.location.href='intent://com.example.boninja/#Intent;scheme=launch;package=com.example.boninja;S.content=WebContent;end'"><span style="
+                        font: 24px/30px IRANSans;margin-top:30px;
+                    " > بازگشت به اپلیکیشن بن اینجا</span>
+               
+        </button>
+                    @endif
+                    
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+	</body>
+
+<!-- Boninja.com -->
+</html>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Resources\Checkout as CheckoutResource;
+use App\BiOrder;
+use App\BiOrderItem;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,9 +46,168 @@ Route::get('/alltraining', 'ApiController@allfetchTraining')->name('Api.allfetch
 Route::get('/allcinema', 'ApiController@allfetchCinema')->name('Api.allfetchCinema');
 Route::get('/allservice', 'ApiController@allfetchService')->name('Api.allfetchService');
 Route::get('/allshops', 'ApiController@allfetchShops')->name('Api.allfetchShops');
+Route::get('/sendtobank', 'ApiController@sendToBank')->name('Api.sendToBank');
 
 
 Route::post('children', 'ApiController@fetchChildren');
 Route::post('order', 'ApiController@fetchOrders');
 Route::post('search', 'ApiController@fetchSearch');
+Route::post('checkout', 'ApiController@checkout');
+Route::any('callback/from/bank',function(){
+  //  $allcategories = BiCategory::where('state','MainMenu')->orderBy('sort_order', 'asc')->get();
+    try {
+      $gateway = \Gateway::verify();
+      $trackingCode = $gateway->trackingCode();
+      $refId = $gateway->refId();
+      $cardNumber = $gateway->cardNumber();
+      // dd($trackingCode);
+  
+      $order_status = 'completed';
+      $order = BiOrder::where('ref_id', $refId)->first();
+      $order->update(['status' => $order_status]);     
+      $order_items = BiOrderItem::where('bi_order_id', $order->id)->get();
+      /*
+      if($order->status == 'completed'){
+        if(session()->has('coupon')){
+          $customer_id = session()->get('coupon')['customer_id'];
+          $coupon_id = session()->get('coupon')['coupon_id'];
+          $customerCouponNew = new CustomerBiCoupon;
+          $customerCouponNew->customer_id = $customer_id;
+          $customerCouponNew->bi_coupon_id = $coupon_id;
+          $customerCouponNew->save();
+  
+        }
+        foreach (Cart::content() as $item) {
+          $product = BiProduct::find($item->id);
+          $productSold = (($product->quantity-$product->sold) - $item->qty) >= 0 ? $product->sold + $item->qty : -1 ;
+          if( $productSold >= 0) {
+                  $product->sold = $productSold;
+                  $product->save();     
+              } 
+        }
+      }
+      
+      foreach($order_items as $order_item){ 
+        if(empty($order_item->product->date_available)){ 
+          if(empty($order_item->product->end_date)){
+              if($order_item->product->parent_id != 0){
+                  if(empty($order_item->product->parent->date_available)){
+                  $dbdate = $order_item->product->parent->end_date;
+                  $dbdate = date('Y/m/d', strtotime($dbdate));
+                  $dbdate = date('Y/m/d', strtotime($dbdate. ' + 10 days'));
+                  $ydate = date('Y', strtotime($dbdate));  
+                  $mdate = date('m', strtotime($dbdate));  
+                  $ddate = date('d', strtotime($dbdate)); 
+                  $date = g2p($ydate,$mdate ,$ddate); 
+                  }else{
+                  $dbdate = $order_item->product->parent->date_available;
+                  $dbdate = date('Y/m/d', strtotime($dbdate));
+                  //$dbdate = date('Y/m/d', strtotime($dbdate. ' + 10 days'));
+                  $ydate = date('Y', strtotime($dbdate));  
+                  $mdate = date('m', strtotime($dbdate));  
+                  $ddate = date('d', strtotime($dbdate)); 
+                  $date = g2p($ydate,$mdate ,$ddate);
+                  }  
+              }
+          }else{
+              $dbdate = $order_item->product->end_date;
+              $dbdate = date('Y/m/d', strtotime($dbdate));
+              $dbdate = date('Y/m/d', strtotime($dbdate. ' + 10 days'));
+              $ydate = date('Y', strtotime($dbdate));  
+              $mdate = date('m', strtotime($dbdate));  
+              $ddate = date('d', strtotime($dbdate)); 
+              $date = g2p($ydate,$mdate ,$ddate);
+          }
+        }else{
+          $dbdate = $order_item->product->date_available;
+          $dbdate = date('Y/m/d', strtotime($dbdate));
+          //$dbdate = date('Y/m/d', strtotime($dbdate. ' + 10 days'));
+          $ydate = date('Y', strtotime($dbdate));  
+          $mdate = date('m', strtotime($dbdate));  
+          $ddate = date('d', strtotime($dbdate)); 
+          $date = g2p($ydate,$mdate ,$ddate);
+        }
+  
+        try{
+          $sms = \Melipayamak::sms();
+          $to = Auth::guard('customer')->user()->phone;
+         
+          $from = '200020001090';
+          if(!empty($order_item->product->parent->name)){
+            $text = $order_item->name."\n".$order_item->product->parent->name."\n"."تعداد : ".$order_item->quantity."\n"."کد تخفیف شما : ".$order_item->code."\n"."مهلت استفاده : ".$date[0]."/".$date[1]."/".$date[2]."\n"."سامانه خريد و تخفيف گروهی بن اينجا"."\n"."https://www.boninja.com/my-account";
+  
+          }else{
+            $text = $order_item->name."\n"."تعداد : ".$order_item->quantity."\n"."کد تخفیف شما : ".$order_item->code."\n"."مهلت استفاده : ".$date[0]."/".$date[1]."/".$date[2]."\n"."سامانه خريد و تخفيف گروهی بن اينجا"."\n"."https://www.boninja.com/my-account";
+          }
+          
+         // $text = $order_item->product->parent->name;
+          $response = $sms->send($to,$from,$text);
+          $json = json_decode($response);
+          //dd($json);
+          //echo $json->Value; //RecId or Error Number 
+        }catch(Exception $e){
+          echo $e->getMessage();
+        }    
+      }
+      $wallet_status = 'completed';
+      
+      $id = Auth::guard('customer')->user()->id;
+      
+      $wallet_last = Wallet::where('customer_id', $id)->where('ref_id', $refId)->orderBy('id','desc')->first();
+      //dd($wallet_last);
+      if(!empty($wallet_last)){
+        $wallet_last->status = $wallet_status;     
+        $wallet_last->tracking_code = $trackingCode;     
+        $wallet_last->save();
+      }
+      $total = Cart::subtotal();
+      $portion = floor($total/10000);
+      if($portion > 0){
+        $score = Score::firstOrNew(['customer_id' => Auth::guard('customer')->user()->id]);
+        $score->value = $score->value + (5*$portion);
+        $score->save();
+    
+        $invitation = Invitation::where('customer_id',Auth::guard('customer')->user()->id)->first();
+        if(!empty($invitation->code)){
+            $inviter = Customer::where('invitation_code', $invitation->code)->first();
+            if(!empty($inviter->id)){
+                $score = Score::firstOrNew(['customer_id' => $inviter->id]);
+                $score->value = $score->value + (5*$portion);
+                $score->save();
+            }
+        }
+      } 
+      Cart::destroy();  
+      if(session()->has('coupon')){
+        session()->forget('coupon');
+      }*/
+      
+      $message = 'پرداخت با موفقیت انجام شد!<br> 
+       کد پیگیری بانکی شما : '.$trackingCode.' <br>
+       برای پیگیری های  بانکی بعدی این کد را نزد خود نگه دارید. 
+       ';
+       return view('layouts/api/callback')->with([
+        
+        'message' => $message
+        ]);
+  /*
+       return view('layouts/checkout/bankresult')->with([
+        'allcategories' => $allcategories,
+        'message' => $message
+        ]);
+    */
+   // return CheckoutResource::collection($message);
 
+    } catch (Exception $e) {
+      $error = $e->getMessage();return view('layouts/api/callback')->with([
+        
+        'error' => $error
+        ]);
+     /* return view('layouts/checkout/bankresult')->with([
+        'allcategories' => $allcategories,
+        'error' => $error
+        ]);*/
+      //  dd($error);
+       // return CheckoutResource::collection($error);
+    }
+  });
